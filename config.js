05e0cac2e6d32382562e9c39da7c76725fe82bdf
Hoan Thien <ginothien05@gmail.com>
2021-09-16 23:08:40 +0700

    first

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: '金银',
+    introDesc: `想对你说句晚安`,
+    btnIntro: 'hihi',
+    title: 'Dù là chuyện gì sẽ xảy ra tiếp theo đi nữa 😙',
+    desc: 'Thì hãy luôn vui vẻ nhé đừng có mà buồn nữa ',
+    btnYes: 'N làm được mà <33',
+    btnNo: 'Không, Vẫn cứ là thích buồn đó :3',
+    question:'Ở một mình đôi khi thật tệ, nhưng sắp được bình yên rồi',
+    btnReply: 'Gửi cho T <3',
+    reply: 'T không nghĩ là T lại có thể làm được mấy vụ này nữa kaka <33333333',
+    mess: '想对你说句晚安🥰.',
+    messDesc: 'Làm bài buổi tối thì nhớ mà ngủ sớm cho đủ giấc nhá',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://fb.com/DHT.Official.24899' 
+}
